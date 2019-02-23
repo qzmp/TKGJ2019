89d2ef13ac8de81ff7df2dde7817f83f5a77db35
kam-l <45072598+kam-l@users.noreply.github.com>
2019-02-23 17:56:47 +0100

    fog spell

---
+++ Assets/Scripts/FogSpell.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogSpell : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _fogPrefab;
+
+    [SerializeField]
+    private Player _player;
+
+    [SerializeField]
+    private float _fogCost;
+
+    [SerializeField]
+    private float _fogDistance;
+
+    [SerializeField]
+    private float _fogCount;
+
+    [SerializeField]
+    private float _fogMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player.mana > _fogCost && Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, LayerMask.GetMask("Background")))
+            {
+                if (Vector3.Distance(transform.position, hit.point) < _fogDistance)
+                {
+                    SpawnFog(hit);
+                }
+            }
+        }
+    }
+
+    public void SpawnFog(RaycastHit raycastHit)
+    {
+        //if (mana > _fogCost)
+        {
+            _player.mana -= _fogCost;
+            Instantiate(_fogPrefab, raycastHit.point, Quaternion.identity);
+        }
+    }
+}
