14df1103374b8394d7fdafc7a89ad7d5e763f51e
Dzejkop <jakubtrad@gmail.com>
2019-02-23 18:37:40 +0100

    Moving player implementation to Assets/Scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 
-public class _Player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField]
     private Animator _legsAnimator;
@@ -14,11 +15,35 @@ public class _Player : MonoBehaviour
     [SerializeField]
     private Transform _topTransform;
 
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _dashSpeed;
+
+    [SerializeField]
+    private float _dashCooldown;
+    private float _lastDashTime;
+
+    [SerializeField]
+    private float _dashCost;
+
+    [SerializeField]
+    public float mana;
+
+    [SerializeField]
+    private float _manaRegeneration;
+
+    private Vector3 _velocity;
+    private bool _doDash;
+
     void Start()
     {
         Assert.IsNotNull(_legsAnimator);
         Assert.IsNotNull(_legsTransform);
         Assert.IsNotNull(_topTransform);
+
+        _lastDashTime = -_dashCooldown;
     }
 
     Vector2 GetDirection()
@@ -35,29 +60,68 @@ public class _Player : MonoBehaviour
 
     void Update()
     {
-        Vector2 direction = Vector2.up;
+        Vector3 dirUp = Vector2.up;
         Vector2 movementInput = GetMovementInput();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float movementAngle = Vector2.SignedAngle(dirUp, movementInput);
 
         _legsAnimator.SetBool("is_walking", movementInput.magnitude > 0);
 
-        float angle = Vector2.SignedAngle(direction, movementInput);
-
-        if (Mathf.Abs(angle) > 90f)
+        if (Mathf.Abs(movementAngle) > 90f)
         {
             _legsAnimator.SetFloat("walking_speed", -movementInput.magnitude);
-            _legsTransform.localRotation = Quaternion.Euler(0, 0, 180.0f + angle);
+            _legsTransform.localRotation = Quaternion.Euler(0, 0, 180.0f + movementAngle);
         }
         else
         {
             _legsAnimator.SetFloat("walking_speed", movementInput.magnitude);
-            _legsTransform.localRotation = Quaternion.Euler(0, 0, angle);
+            _legsTransform.localRotation = Quaternion.Euler(0, 0, movementAngle);
         }
 
-        #region Kamil
-        Vector3 flatMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(flatMousePos + " | " + _topTransform.position);
-        flatMousePos.z = 128;
-        _topTransform.LookAt(flatMousePos, Vector3.back);
-        #endregion
+        worldMousePosition.z = 128;
+        _topTransform.LookAt(worldMousePosition, Vector3.back);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _doDash = true;
+        }
+
+        mana = Mathf.Clamp(mana + _manaRegeneration * Time.deltaTime, 0.0f, 1.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        Vector3 dirUp = Vector2.up;
+        Vector2 movementInput = GetMovementInput();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float movementAngle = Vector2.SignedAngle(dirUp, movementInput);
+
+        if (GetMovementInput().magnitude > 0)
+        {
+            if (_dashCooldown > 0) //Wariant: cooldown dasha
+            {
+                if (_doDash && Time.time < _lastDashTime + _dashCooldown)
+                {
+                    _doDash = false;
+                }
+                else if (_doDash)
+                {
+                    _lastDashTime = Time.time;
+                    Debug.Log(_lastDashTime);
+                }
+            }
+
+            if(_dashCost > 0) //Wariant: mana
+            {
+                if (_doDash && mana < _dashCost)
+                    _doDash = false;
+                else if (_doDash)
+                    mana -= _dashCost;
+            }
+
+            rigidbody.AddForce(Quaternion.Euler(0, 0, movementAngle) * dirUp * (_doDash ? _dashSpeed : _speed), (_doDash ? ForceMode2D.Impulse : ForceMode2D.Force));
+            _doDash = false;
+        }
     }
 }
